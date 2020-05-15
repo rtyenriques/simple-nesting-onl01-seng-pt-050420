c9356f29a0feae314980429a8b200657e872bcfa
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-15 21:31:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require pry
 
 def hopper
 	programmer_hash =
@@ -130,5 +131,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+binding.pry
+     
 programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 end
